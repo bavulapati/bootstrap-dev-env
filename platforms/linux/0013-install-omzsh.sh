f25c93e241e754339d8acd745189c5822ae1ca04
Balakrishna Avulapati <bavulapati@gmail.com>
2022-09-11 00:28:26 +0530

    Do not reinstall omz if it's configuration is already set

--- platforms/linux/0013-install-omzsh.sh
+++ platforms/linux/0013-install-omzsh.sh
@@ -3,9 +3,17 @@
 set -o errexit
 set -o nounset
 
+OMZ_FILE="$HOME/.oh-my-zsh"
+
+if [ -d "$OMZ_FILE" ]
+then
+	exit 0
+fi
+
 if [ -x "$(command -v omz)" ]
 then
 	exit 0
 fi
 
+echo "installing omz"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
