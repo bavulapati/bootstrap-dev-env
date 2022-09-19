f376c56ef8741e5c59fd88680e97247fee2eb063
Balakrishna Avulapati (Bala) <bavulapati@gmail.com>
2022-09-19 16:43:42 +0530

    Edit git config to include comment about name

--- platforms/linux/0004-configure-git.sh
+++ platforms/linux/0004-configure-git.sh
@@ -5,4 +5,4 @@ set -o nounset
 
 # Configure git
 git config --global  user.email "bavulapati@gmail.com"
-git config --global  user.name "Balakrishna Avulapati"
+git config --global  user.name "Balakrishna Avulapati (Bala)"
