f9350d6f970c24ed1ad8b98ac0c7f721d1e7a366
Balakrishna Avulapati (Bala) <bavulapati@gmail.com>
2022-11-16 15:03:47 +0530

    Install ninja on Darwin

--- platforms/Darwin/0005-install-powerlevel10k.sh
+++ platforms/Darwin/0005-install-powerlevel10k.sh
@@ -3,6 +3,6 @@
 set -o nounset
 set -o errexit
 
-// TODO: Skip p10k installation on detection
+# TODO: Skip p10k installation on detection
 HOMEBREW_NO_ANALYTICS=1 HOMEBREW_NO_AUTO_UPDATE=1 \
   brew install romkatv/powerlevel10k/powerlevel10k 
