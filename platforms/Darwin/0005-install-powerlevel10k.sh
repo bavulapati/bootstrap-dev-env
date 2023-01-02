717c3b665aa97b596f27c17784cfcedc61388536
Balakrishna Avulapati <bavulapati@gmail.com>
2023-01-02 12:45:30 +0530

    Use Brewfile for powerlevel10k

--- platforms/Darwin/0005-install-powerlevel10k.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -o nounset
-set -o errexit
-
-# TODO: Skip p10k installation on detection
-HOMEBREW_NO_ANALYTICS=1 HOMEBREW_NO_AUTO_UPDATE=1 \
-  brew install romkatv/powerlevel10k/powerlevel10k 
